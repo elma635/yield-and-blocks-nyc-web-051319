20ce86073af69c28689b9c8b4d61b9baeef534e5
Elma Rahman <elmarahman635@gmail.com>
2019-04-17 02:12:07 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,11 +8,7 @@ def hello_t(array)
   end
   array
 else
-  puts "Hey! No block was given"
-
-hello_t(["Tim", "Tom", "Jim"]) do | name |
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  puts "Hey! No block was given!"
   end
 end
 # call your method here!
